23c289260c8c22d524de2a4c7326669bc5cec274
Shekar Siri <sshekarsiri@gmail.com>
2023-08-25 18:06:46 +0530

    feat(ui): admin console

--- frontend/app/components/AdminConsole/AdminConsoleContent/Maintenance/Maintenance.tsx
+++ frontend/app/components/AdminConsole/AdminConsoleContent/Maintenance/Maintenance.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import { Space, Tag, Typography } from 'antd';
+import { CloudUploadOutlined, CodeSandboxOutlined } from '@ant-design/icons';
+
+const { Text } = Typography;
 
 function Maintenance() {
   return (
-    <div></div>
+    <div>
+      <div>
+        <Space>
+          <CloudUploadOutlined />
+          <Text>Version</Text>
+        </Space>
+
+
+      </div>
+    </div>
   );
 }
 
